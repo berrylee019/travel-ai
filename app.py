f066a676b990457fc68b4d48e7abe1699b801173
Lee ByungSeo <bslee@yahoo.com>
2026-07-16 21:59:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def get_client():
         creds_dict = json.loads(raw_data)
     else:
         creds_dict = raw_data
-    return creds_dict
+    #return creds_dict
     
     # 3. 권한 범위 설정
     scope = [
